441ef28dbbeedbba6fd79ee6caa5416a2f71d348
Glitch (pale-puffy-heat) <none>
2021-03-25 00:22:54 +0000

    🐕🍦 Checkpoint ./commands/withdraw.js:27979027/1785 ./commands/deposit.js:27979027/1851

--- commands/deposit.js
+++ commands/deposit.js
@@ -2,17 +2,19 @@ module.exports = {
   name: "deposit",
   aliases: ['dep'],
   code: `
+  $if[$message==$isNumber]
 $setUserVar[bank;$sum[$getUserVar[bank;$authorID];$message];$authorID]
 $setUserVar[money;$sub[$getUserVar[money;$authorID];$message];$authorID]
 $description[You deposited :dollar: $message in your :bank: bank]
 $author[$username;$authorAvatar]
-$suppressErrors[Not a valid number!]
-$if[$message==all]
-$description[**You have Deposited everything into bank**]
+$suppressError[Not a Vaild Number!]
+$elseif[$message==all]
+$addfield[Deposit;**You have Deposited everything into bank**]
 $setUserVar[bank;$sum[$getUserVar[bank;$authorID];$getUserVar[bank;$authorID]];$authorID]
 $setUserVar[money;$sub[$getServerVar[money;$authorID];$getServerVar[money;$authorID]];$authorID]
+$endelseif
 $endif
 $onlyIf[$message<=$getUserVar[money;$authorID];That amount is higher than the amount in your wallet!]
-$onlyIf[$message>0;You cannot deposit an amount lower than \`0\`. If you want to deposit all, then use the command \`*deposit all\`]
+$onlyIf[$message>0;You cannot deposit an amount lower than \`0\`. If you want to deposit all, then use the command \`s.deposit all\`]
 `
 };
--- commands/withdraw.js
+++ commands/withdraw.js
@@ -7,10 +7,10 @@ $description[You withdrew :dollar: $message from your :bank: bank]
 $author[$username;$authorAvatar]
 $suppressErrors[Not a valid number!]
 $if[$message=all]
-$description[**You have Withdrawn everything from your bank**]
+$addfield[Withdraw;**You have Withdrawn everything from your bank**]
 $setUserVar[bank;$sub[$getUserVar[bank;$authorID];$getUserVar[bank;$authorID]];$authorID]
 $setUserVar[money;$sum[$getServerVar[money;$authorID];$getServerVar[money;$authorID]];$authorID]
 $endif
 $onlyIf[$message<=$getUserVar[bank;$authorID];That amount is higher than the amount in your bank!]
 $onlyIf[$message>0;You cannot withdraw an amount lower than \`0\`. If you want to withdraw all, then use the command s.withdraw all]`
-}
+};
